de3bc1e2c0ec4b1bc30d74e2ecacf467ea0168b0
pon52.lemon <85234696+pon52-lemon@users.noreply.github.com>
2021-06-06 00:08:21 +0900

    2021/06/06 ベース（Hello World）

---
+++ src/main/java/com/gmail/pon52_lemon/App.java
@@ -0,0 +1,13 @@
+package com.gmail.pon52_lemon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
